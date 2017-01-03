10ecf3de2f3d87eeac08afe461bbbbbd6a7a7724
Matthew Goldwater <mattgwater@gmail.com>
2017-01-03 15:28:46 -0800

    Set proper user data as properties on record

--- server/hashPassword.js
+++ server/hashPassword.js
@@ -1,6 +1,8 @@
 const deepstream = require('deepstream.io-client-js');
 const client = deepstream('localhost:6020').login();
+const MongoDBStorageConnector = require( 'deepstream.io-storage-mongodb' );
 const bcrypt = require('bcrypt');
+
 
 const hashPassword = function(body, res) {
 console.log('hits hashPassword function');
@@ -12,16 +14,30 @@ bcrypt.hash(body.password, 10).then(function(results) {
   // res.send(body);
   //this is a server-side client & server
   //check if the record already exists
-  
-  client.record.has('user/' + body.username, function() {
-    console.log('is this hit for some reason?')
+  console.log('body username is: ', body.username);
+  const checkThisRecordName = 'user/' + body.username;
+  console.log('checkThisRecordName is: ', checkThisRecordName);
+  /*client.record.has(checkThisRecordName, function() {
+    console.log('is this hit for some reason?');
     return res.status(403).send('Invalid Credentials');
-  });
+  });*/ //this ran too late. so the record would be created before this ran
+  console.log('runs before is this hit for some reason?')
   //create record here
-  const user = client.record.getRecord('user/' + body.username);
-  console.log('user is: ', user.name);
+  const user = client.record.getRecord('user/' + body.username).whenReady(function(user) {
+    user.set({
+      username: body.username,
+      password: body.password,
+      email: body.email
+      }, function (err) {
+        if (err) {
+          console.log('error is: ', err);
+        } else {
+          console.log('record set without error');
+          console.log('user data is: ', user._$data);
+        }
+      })
+    })
   //run function when record is created to save the record to the mongo database
-  
   //look at http authentication page again 
   //send status code
   return res.status(200).send('200 Ok!');
