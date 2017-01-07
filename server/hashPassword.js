0efdef852da931aff4962cdfec636004355b99ea
Matthew Goldwater <mattgwater@gmail.com>
2017-01-06 17:59:42 -0800

    Emails are checked via querying the database and responses with proper data are sent based on whether the password is correct or incorrect. still need to see if there are timing errors

--- server/hashPassword.js
+++ server/hashPassword.js
@@ -1,3 +1,11 @@
+/*
+            let emailExists = emailRegExp.test(allUsersArr[i]);
+            if (emailExists) {
+              console.log('the email has already been used!')
+              return res.status(403).send('the email has already been used');
+            }
+            */  
+
 const deepstream = require('deepstream.io-client-js');
 const client = deepstream('localhost:6020').login({role: 'provider', username: 'accounts-service', password: '12345'});
 const bcrypt = require('bcrypt');
@@ -5,6 +13,10 @@ const bcrypt = require('bcrypt');
 console.log('will this run when the server starts up?');
 
 function checkForValidLogin(user, res) {
+  res.set({
+    userId: 'users/' + user.username,
+    username: user.username
+  })
   console.log('are headers sent?', res.headersSent)
   console.log('user.username is: ', user.username);
   console.log('hits checkforValidLogin. user is: ', user);
@@ -34,10 +46,15 @@ function checkForValidLogin(user, res) {
       client.record.snapshot(record.name, function(error, data) {
         console.log('data of snapshot is: ', data);
         bcrypt.compare(user.password, data.password).then(function(results){
-        console.log('password is correct');
-        // console.log('res is: ', res);
-        console.log('does this log');
-        res.status(200).send({})
+          if (results) {
+            console.log('password is correct');
+            // console.log('res is: ', res);
+            console.log('does this log');
+            res.status(200).send();
+          } else {
+            console.log('password is incorrect');
+            res.status(403).send();
+          }
       }).catch(function(err){
         console.log('Password is incorrect.', err);
         return res.status(403).end();
@@ -50,63 +67,58 @@ function checkForValidLogin(user, res) {
 module.exports = checkForValidLogin;
 
 const hashPasswordSignUp = function(body, res) {
-console.log('hits hashPasswordSignUp function');
-console.log('body.password is: ', body.password);
-bcrypt.hash(body.password, 10).then(function(results) {
-  console.log('results is: ', results);
-  body.password = results;
-  console.log('body is: ', body);
-  // res.send(body);
-  //this is a server-side client & server
-  //check if the record already exists
-  console.log('body username is: ', body.username);
-  const checkThisRecordName = 'user/' + body.username + '|' + body.email;
-  console.log('checkThisRecordName is: ', checkThisRecordName);
-  client.record.has(checkThisRecordName, function(error, hasRecord) {
-    console.log('if Im reading this then it hit the function to check record names?')
-    if (error) {
-      console.log('is this hit for some reason?');
-      return res.status(403).send('Invalid Credentials');
-    } else if (hasRecord) {
-      console.log('the record already exists!')
-      return res.status(403).send('User already exists');
-    } else {
-      //check for users in list
-      let allUsersList = client.record.getList('allUsersList').whenReady(function(list){
-        let allUsersArr = list.getEntries();
-        for (let i = 0; i < allUsersArr.length; i++) {
-          let usernameRegExp = new RegExp('^user/' + body.username, 'gi');
-          console.log('the usernameRegExp is: ', usernameRegExp);
-          let usernameExists = usernameRegExp.test(allUsersArr[i]);
-          if (usernameExists) {
-            console.log('the username has already been used!');
-            return res.status(403).send('the username has already been used');
+  console.log('hits hashPasswordSignUp function');
+  console.log('body.password is: ', body.password);
+  bcrypt.hash(body.password, 10).then(function(results) {
+    body.password = results;
+    const checkThisRecordName = 'user/' + body.username + '|' + body.email;
+    client.record.has(checkThisRecordName, function(error, hasRecord) {
+      if (error) {
+        return res.status(403).send('Invalid Credentials');
+      } else if (hasRecord) {
+        return res.status(403).send('User already exists');
+      } else {
+        let allUsersList = client.record.getList('allUsersList').whenReady(function(list){
+          let allUsersArr = list.getEntries();
+          for (let i = 0; i < allUsersArr.length; i++) {
+            let usernameRegExp = new RegExp('^user/' + body.username, 'gi');
+            let usernameExists = usernameRegExp.test(allUsersArr[i]);
+            if (usernameExists) {
+              console.log('the username has already been used!');
+              return res.status(403).send('the username has already been used');
+            }
           }
-          let emailRegExp = new RegExp(body.email + '$', 'gi');
-          console.log('the emailRegExp is: ', emailRegExp)
-          let emailExists = emailRegExp.test(allUsersArr[i]);
-          if (emailExists) {
-            console.log('the email has already been used!')
-            return res.status(403).send('the email has already been used');
-          }    
-        }
-        const user = client.record.getRecord('user/' + body.username + '|' + body.email).whenReady(function(user) {
-          user.set('username', body.username);
-          user.set('password', body.password);
-          user.set('email', body.email);
-          list.addEntry(checkThisRecordName);
-          console.log('new user has been created and added to the list');
-          return res.status(200).send('200 Ok!');
-        })
+          // let emailRegExp = new RegExp(body.email + '$', 'gi');
+          let findEmail = JSON.stringify({
+            table: 'user',
+            query: [
+              ['email', 'eq', body.email]
+            ]
+          })
+          let queryResults = client.record.getList('search?' + findEmail);
+          queryResults.whenReady(function(list) {
+            let emailTaken = list.getEntries();
+            if (emailTaken.length !== 0) {
+              return res.status(403).send('the email has already been used');
+            }  
+          })
+          const user = client.record.getRecord('user/' + body.username).whenReady(function(user) {
+            user.set('username', body.username);
+            user.set('password', body.password);
+            user.set('email', body.email);
+            list.addEntry(checkThisRecordName);
+            console.log('new user has been created and added to the list');
+            return res.status(200).send('200 Ok!');
+          })
       })
-    }
-  })
+}
 }).catch(function(err) {
-  console.log('Promise rejected. Error is: ', err);
-});
+    console.log('Promise rejected. Error is: ', err);
+  });
+})
 }
 
 module.exports = {
   hashPasswordSignUp: hashPasswordSignUp,
   checkForValidLogin: checkForValidLogin
-};
+}
