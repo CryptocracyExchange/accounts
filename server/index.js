5b85a3408e2aabd500a12a8e9571c1690eb7fabe
Matthew Goldwater <mattgwater@gmail.com>
2017-01-12 16:42:12 -0800

    Clean up of login post code

--- server/index.js
+++ server/index.js
@@ -23,16 +23,10 @@ app.get('/login', function (req,res) {
 
 app.post('/login', function (req, res) {
   console.log('if I see this then a post request was made to login');
-  // console.log('req.body is: ', req.body);
   console.log('req.body.authData.role is: ', req.body.authData.role);
   if (req.body.authData.role === 'user') {
     console.log('a user is trying to login');
-    //req.body.authData.username'
-    /*return new Promise(function(resolve, reject){
-      console.log('promise function starts')
-      resolve();
-    }).then()*/
-      signUpOrLogIn.checkLogin(results, req, res);
+    signUpOrLogIn.checkLogin(results, req, res);
   } else if (req.body.authData.role === 'provider') {
     console.log('a provider is trying to login');
     return res.status(200).end();
@@ -41,23 +35,6 @@ app.post('/login', function (req, res) {
     return res.status(403).end();
   }
 })
-/*
-app.post('/login', function (req, res) {
-  console.log('if I see this then a post request was made to login');
-  // console.log('req.body is: ', req.body);
-  console.log('req.body.authData.role is: ', req.body.authData.role);
-  if (req.body.authData.role === 'user') {
-    console.log('a user is trying to login');
-    signUpOrLogIn.checkForValidLogin(req.body.authData, res);
-    // return res.status(200).end();
-  } else if (req.body.authData.role === 'provider') {
-    console.log('a provider is trying to login');
-    return res.status(200).end();
-  } else {
-    console.log('invalid request')
-    return res.status(403).end();
-  }
-})*/
 
 app.post('/signup', function (req, res) {
   console.log('a post request happened');
