05e9f461200f1aa72fab48e28b37647519b3bfd4
Mikel Duffy <mikelduffy1@gmail.com>
2017-01-18 09:22:35 -0800

    Adds init balance on new user creation

--- server/loginAndSignup.js
+++ server/loginAndSignup.js
@@ -132,54 +132,31 @@ Provider.prototype.checkLogin = function (results, req, res) {
 };
 
 Provider.prototype.signUp = function (body, res) {
-  // check if user exists
-  console.log('gets into signUp function');
   const checkForDuplicateUser = new Promise((resolve, reject) => {
     this._deepstreamClient.record.snapshot(`user/${body.username}`, (error, data) => {
-      console.log('user: error is', error, 'data is ', data);
       if (error) {
-        console.log('hits error: ', error);
         resolve();
       }
       if (data) {
-        console.log('user: hits data', data);
-        // res.status(403).send('Username already taken');
-        reject('username');
+        reject(`${body.username} already exists`);
       }
     });
-  })
-  /*.catch((results) => {
-    console.log('checkForDuplicateUser is: ', results);
   });
-  */
-    // if yes return 403 Invalid Credentials
-  // check if email exists
+
   const checkForDuplicateEmail = new Promise((resolve, reject) => {
     this._deepstreamClient.record.snapshot(`email/${body.email}`, (error, data) => {
-      console.log('email: error is ', error, 'data is ', data);
       if (error) {
-        console.log('hits error: ', error);
         resolve();
       }
       if (data) {
-        console.log('email: hits data', data);
-        // res.status(403).send('Email already taken');
-        reject('email');
+        reject(`${body.email} already exists`);
       }
     });
-  })
-/*
-  .catch((results) => {
-    console.log('checkForDuplicateEmail results is: ', results);
   });
-*/
+
   Promise.all([checkForDuplicateUser, checkForDuplicateEmail]).then(() => {
-    console.log('New username and new email!');
     bcrypt.genSalt(10, (error, salt) => {
-      console.log('salt is: ', salt);
       bcrypt.hash(body.password, salt, null, (err, hashedPassword) => {
-        console.log('hashed password is: ', hashedPassword);
-        // body.password = hashedPassword;
         this._deepstreamClient.record.getRecord(`user/${body.username}`).whenReady((newUserRecord) => {
           newUserRecord.set('username', body.username);
           newUserRecord.set('password', hashedPassword);
@@ -190,79 +167,13 @@ Provider.prototype.signUp = function (body, res) {
           newEmailRecord.set('password', hashedPassword);
           res.status(200).send('new user has been created');
         });
+        this._deepstreamClient.event.emit('initBalance', { userID: body.username });
       });
     });
   }).catch((reason) => {
-    console.log('Promise.all rejection reason is: ', reason);
+    this.log(`New account - Promise rejection: ${reason}`);
     res.status(403).send(`${reason} is already taken`);
   });
 };
 
 module.exports = Provider;
-
-  // if yes return 403 Invalid Credentials
-// if neither exists
-  // generate salt
-  // hashPassword
-  // create user record with username, password, email
-  // create email record with email, password
-/*
-const hashPasswordSignUp = function(body, res) {
-  console.log('hits hashPasswordSignUp function');
-  console.log('body.password is: ', body.password);
-  bcrypt.genSalt(10, function (error, salt) {
-    console.log('salt is: ', salt)
-    bcrypt.hash(body.password, salt, null, function(error, results) {
-      console.log('results is', results);
-      body.password = results;
-      const checkThisRecordName = 'user/' + body.username;
-      client.record.has(checkThisRecordName, function(error, hasRecord) {
-        if (error) {
-          return res.status(403).send('Invalid Credentials');
-        } else if (hasRecord) {
-          return res.status(403).send('User already exists');
-        } else {
-          let allUsersList = client.record.getList('allUsersList').whenReady(function(list){
-            let allUsersArr = list.getEntries();
-            for (let i = 0; i < allUsersArr.length; i++) {
-              let usernameRegExp = new RegExp('^user/' + body.username, 'gi');
-              let usernameExists = usernameRegExp.test(allUsersArr[i]);
-              if (usernameExists) {
-                console.log('the username has already been used!');
-                return res.status(403).send('the username has already been used');
-              }
-            }
-            // let emailRegExp = new RegExp(body.email + '$', 'gi');
-            let findEmail = JSON.stringify({
-              table: 'user',
-              query: [
-                ['email', 'eq', body.email]
-              ]
-            })
-            let queryResults = client.record.getList('search?' + findEmail);
-            queryResults.subscribe
-            queryResults.whenReady(function(list) {
-              let emailTaken = list.getEntries();
-              if (emailTaken.length !== 0) {
-                // list.discard();
-                res.status(403).send('the email has already been used');
-              } else {
-                // list.discard();
-                const user = client.record.getRecord('user/' + body.username).whenReady(function(user) {
-                user.set('username', body.username);
-                user.set('password', body.password);
-                user.set('email', body.email);
-                list.addEntry(checkThisRecordName);
-                console.log('new user has been created and added to the list');
-                return res.status(200).send('200 Ok!');
-            })
-              }
-              return;  
-            })
-        })
-        }
-      })
-    })
-  });
-}
-*/
